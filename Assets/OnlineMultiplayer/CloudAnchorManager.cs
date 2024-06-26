0a52062e63583aca21a9dce3ba9ed684d97f696f
Muhammad Behzad Hussain <muhammad-behzad.hussain@tu-ilmenau.de>
2024-06-27 01:37:15 +0200

    Creation of cloud anchor class is updated

--- Assets/OnlineMultiplayer/CloudAnchorManager.cs
+++ Assets/OnlineMultiplayer/CloudAnchorManager.cs
@@ -1,4 +1,5 @@
 public class CloudAnchorManager : MonoBehaviour
+private Dictionary<int, CloudAnchor> cloudAnchors = new Dictionary<int, CloudAnchor>();
 {
     public GameManager gameManager;
 
@@ -20,7 +21,29 @@ public class CloudAnchorManager : MonoBehaviour
 
     void CreateCloudAnchor(int playerId)
     {
-        // Create a new cloud anchor for the player
-        //...
+         CloudAnchor cloudAnchor = gameManager.tokenParent.GetComponentInChildren<CloudAnchor>();
+          if (cloudAnchor == null)
+        {
+            cloudAnchor = gameManager.tokenPrefab.GetComponent<CloudAnchor>();
+        }
+
+        // Set the cloud anchor's ID and player ID
+        cloudAnchor.anchorId = Guid.NewGuid().ToString();
+        cloudAnchor.playerId = playerId;
+
+        // Add the cloud anchor to the dictionary
+        cloudAnchors.Add(playerId, cloudAnchor);
     }
+     public void OnPlayerJoined(int playerId)
+    {
+        // Create a new cloud anchor for the player
+            CreateCloudAnchor(playerId);
+    }
+
+    public void OnPlayerLeft(int playerId)
+    {
+        // Remove the cloud anchor for the player
+        RemoveCloudAnchor(playerId);
+    }
+
 }
